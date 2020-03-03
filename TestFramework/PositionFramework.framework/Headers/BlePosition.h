98f09d720548bd821edf6e3812e4b5bca0949b2f
忘词 <420197925@qq.com>
2020-03-03 16:16:18 +0800

    修改了扫描不到iBeacon空指针的问题

--- TestFramework/PositionFramework.framework/Headers/BlePosition.h
+++ TestFramework/PositionFramework.framework/Headers/BlePosition.h
@@ -15,10 +15,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BlePosition : NSObject<CLLocationManagerDelegate>
 
+/// 必需调用的初始化方法，不能使用init
+/// @param path 指纹库的相对路径
 - (instancetype)initWithFinger:(NSString *)path;
 
+/// 开始定位的方法
+/// @param callback 定位结果的回调，loc为二维NSArray，loc[0]为x，loc[1]为y
 - (void)startWithBlock:(void(^)(NSArray* loc))callback;
 
+/// 停止定位
 - (void)stop;
 @end
 
